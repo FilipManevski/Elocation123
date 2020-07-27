c94faff1bc5484fd46b6b2f200bc17b54e384658
Zocozoid <zoran-90@hotmail.com>
2020-07-27 14:49:30 +0200

    Changes in main_layout

--- app/src/main/java/com/example/newelocationapp/MainActivity.java
+++ app/src/main/java/com/example/newelocationapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.newelocationapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +11,24 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
 
+Button client;
+
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-
+        client = findViewById(R.id.button5);
+        client.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SecondPage.class);
+                startActivity(i);
+            }
+        });
 
 
 
